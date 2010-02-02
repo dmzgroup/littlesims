8e030669947ecbf4077851f48d3a2f54da400a10
Randall E. Barker <simstorm@mac.com>
2010-02-01 16:23:53 -0800

    Initial config checkin

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. ../scripts/envsetup.sh
+
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/canvas.xml config/common.xml config/input.xml config/lua.xml config/resource.xml config/runtime.xml config/version.xml
