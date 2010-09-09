69454754dcfdf5afad8cbc7013d2965e2ae11288
Randall E. Barker <simstorm@mac.com>
2010-09-09 16:06:43 -0700

    Updated mac installer build script.

--- installers/build_mac_app.sh
+++ installers/build_mac_app.sh
@@ -5,6 +5,7 @@ cp -RL $DEPTH/bin/macos-opt/littlesims.app $DEPTH
 mkdir $DEPTH/littlesims.app/Contents/Frameworks/Qt
 mkdir $DEPTH/littlesims.app/Contents/Frameworks/Qt/plugins
 mkdir $DEPTH/littlesims.app/Contents/Frameworks/Qt/plugins/imageformats
+mkdir $DEPTH/littlesims.app/Contents/Frameworks/v8
 cp $DEPTH/depend/Qt/QtCore $DEPTH/littlesims.app/Contents/Frameworks/Qt
 cp $DEPTH/depend/Qt/QtGui $DEPTH/littlesims.app/Contents/Frameworks/Qt
 cp $DEPTH/depend/Qt/QtOpenGL $DEPTH/littlesims.app/Contents/Frameworks/Qt
@@ -17,6 +18,7 @@ cp $DEPTH/depend/Qt/libqsvg.dylib $DEPTH/littlesims.app/Contents/Frameworks/Qt/p
 if [ -d $DEPTH/depend/QtGui.framework/Versions/4/Resources/qt_menu.nib ] ; then
 cp -R $DEPTH/depend/QtGui.framework/Versions/4/Resources/qt_menu.nib $DEPTH/littlesims.app/Contents/Resources
 fi
+cp $DEPTH/depend/v8/lib/libv8.dylib $DEPTH/littlesims.app/Contents/Frameworks/v8
 TARGET=$DEPTH/littlesims-`cat $DEPTH/tmp/macos-opt/mbraapp/buildnumber.txt`.dmg
 hdiutil create -srcfolder $DEPTH/littlesims.app $TARGET
 hdiutil internet-enable -yes -verbose $TARGET
