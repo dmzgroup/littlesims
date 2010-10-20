91b0133725a51251b41056f44b9811ba2274ffb2
Randall E. Barker <simstorm@mac.com>
2010-10-20 11:26:32 -0700

    Added app name env var.

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 . ../scripts/envsetup.sh
 
+export DMZ_APP_NAME=littlesims
 $RUN_DEBUG$BIN_HOME/dmzAppQt -f config/canvas.xml config/common.xml config/input.xml config/js.xml config/resource.xml config/runtime.xml config/version.xml $*
